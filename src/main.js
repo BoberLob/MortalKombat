29aa1a22de06883d63a0f7d35edfd23d8106c004
BoberLob <55806787+BoberLob@users.noreply.github.com>
2021-10-15 00:38:45 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,32 +4,33 @@ import gamers from './assets/db.json'
 
 const $arenas = document.querySelector('.arenas');
 
-const player1 = {
-    player: 1,
-    name: 'Kitana',
-    hp : 100,
-    img : 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
-    weapon: ['brain', 'look', 'voice'],
-    function attack() {
-        console.log(this.name + " fight...");
-    }
-}
-const player2 = {
-    player: 2,
-    name: 'Sonya',
-    hp : 100,
-    img : 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
-    weapon: ['brain', 'look', 'voice'],
-    function attack() {
-        console.log(this.name + " fight...");
-    }
-}
 const player1 = new Player(gamers[0]);
+arenas.appendChild(createPlayer(player1))
+
+arenas.appendChild(createPlayer(player2))
 const player2 = new Player(gamers[4]);
 
-arenas.appendChild(createPlayer(player1))
-arenas.appendChild(createPlayer(player2))
 
+// const player1 = {
+//     player: 1,
+//     name: 'Kitana',
+//     hp : 100,
+//     img : 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+//     weapon: ['brain', 'look', 'voice'],
+//     function attack() {
+//         console.log(this.name + " fight...");
+//     }
+// }
+// const player2 = {
+//     player: 2,
+//     name: 'Sonya',
+//     hp : 100,
+//     img : 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+//     weapon: ['brain', 'look', 'voice'],
+//     function attack() {
+//         console.log(this.name + " fight...");
+//     }
+// }
 // <div class="player1">
 //     <div class="progressbar">
 //         <div class="life"></div>
@@ -39,7 +40,6 @@ arenas.appendChild(createPlayer(player2))
 //         <img src="http://reactmarathon-api.herokuapp.com/assets/scorpion.gif" />
 //     </div>
 // </div>
-
 // http://reactmarathon-api.herokuapp.com/assets/kitana.gif
 // http://reactmarathon-api.herokuapp.com/assets/subzero.gif
 // http://reactmarathon-api.herokuapp.com/assets/sonya.gif
