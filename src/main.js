9bef0af943c3fd7ee70401f96e6c57599ad96915
BoberLob <55806787+BoberLob@users.noreply.github.com>
2021-10-14 21:32:21 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log('Hi!!!')
+
+<div class="player1">
+    <div class="progressbar">
+        <div class="life"></div>
+        <div class="name">SCORPION</div>
+    </div>
+    <div class="character">
+        <img src="http://reactmarathon-api.herokuapp.com/assets/scorpion.gif" />
+    </div>
+</div>
+http://reactmarathon-api.herokuapp.com/assets/subzero.gif
+http://reactmarathon-api.herokuapp.com/assets/sonya.gif
+http://reactmarathon-api.herokuapp.com/assets/liukang.gif
+http://reactmarathon-api.herokuapp.com/assets/liukang.gif
+http://reactmarathon-api.herokuapp.com/assets/scorpion.gif
