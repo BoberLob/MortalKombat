a4d72d3f83c05684b9b418d661037a45bf1e9a17
BoberLob <55806787+BoberLob@users.noreply.github.com>
2021-10-14 23:20:38 +0300

    goto func createPlayer to ./js/createPlayer.js

--- src/main.js
+++ src/main.js
@@ -21,38 +21,6 @@ const player2 = {
     }
 }
 
-function createElem(tag, className) {
-    const $tag = document.createElement(tag);
-    if (className) {
-        $tag.classList.add(className);
-    }
-    return $tag;
-}
-
-function createPlayer(playerObj) {
-    
-    const $player = createElem('div', 'player' + playerObj.player);
-    const $progressbar = createElem('div', 'progressbar');
-    const $character = createElem('div', 'character');
-    const $life = createElem('div', 'life');
-    const $name = createElem('div', 'name');
-    const $img = createElem('img');
-    
-    $life.style.width = playerObj.hr + "%";
-    $name.innerText = playerObj.name;
-    $img.src = playerObj.img;
-
-    $player.appendChild($progressbar);
-    $player.appendChild($character);
-    $progressbar.appendChild($life);
-    $progressbar.appendChild($name);
-    $character.appendChild($img);
-    
-   return $player;
-};
-
-
-
 arenas.appendChild(createPlayer(player1))
 arenas.appendChild(createPlayer(player2))
 
