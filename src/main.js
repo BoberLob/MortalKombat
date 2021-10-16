0bbc66138d29504989083fcca6bb4b5a007fc351
BoberLob <55806787+BoberLob@users.noreply.github.com>
2021-10-17 00:04:18 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,39 +6,48 @@ const $arenas = document.querySelector('.arenas');
 const randomButton = document.querySelector('.button');
 
 const player1 = new Player(gamers[0]);
-arenas.appendChild(createPlayer(player1))
-
 const player2 = new Player(gamers[4]);
+
+function playerWin(name) {
+    const winTitle = createElem('div', 'loseTitle');
+    winTitle.innerHTML = `${name} win!`;
+    return winTitle;
+}
+
+function gameOver() {
+    if (player1.hp < 0 && player2.hp > 0) {
+        player1.hp = 0;
+        randomButton.disabled = true;
+        arenas.appendChild(playerWin(player2.name));
+    }
+    if (player1.hp > 0 && player2.hp < 0) {
+        player2.hp = 0;
+        randomButton.disabled = true;
+        arenas.appendChild(playerWin(player1.name));
+    }
+    if (player1.hp < 0 && player2.hp < 0) {
+        let nobody = 'Nobody wins!';
+        player1.hp = 0;
+        player2.hp = 0;
+        randomButton.disabled = true;
+        arenas.appendChild(playerWin(nobody));
+    }
+}
+
+function changePlayer(player) {
+    const playerLife = document.querySelector(`.player${player.playerNumber} .life`);
+    player.hp -= Math.ceil(Math.random() * 20);
+    playerLife.style.width = player.hp <= 0 ? `0%` : `${player.hp}%`;
+    if (player.hp < 0) {
+        gameOver();
+    }
+
+}
+
+randomButton.addEventListener('click', () => {
+    changePlayer(player1);
+    changePlayer(player2);
+});
+
 arenas.appendChild(createPlayer(player2))
-
-
-
-// const player1 = {
-//     player: 1,
-//     name: 'Kitana',
-//     hp : 100,
-//     img : 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
-//     weapon: ['brain', 'look', 'voice'],
-//     function attack() {
-//         console.log(this.name + " fight...");
-//     }
-// }
-// const player2 = {
-//     player: 2,
-//     name: 'Sonya',
-//     hp : 100,
-//     img : 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
-//     weapon: ['brain', 'look', 'voice'],
-//     function attack() {
-//         console.log(this.name + " fight...");
-//     }
-// }
-// <div class="player1">
-//     <div class="progressbar">
-//         <div class="life"></div>
-//         <div class="name">SCORPION</div>
-//     </div>
-//     <div class="character">
-//         <img src="http://reactmarathon-api.herokuapp.com/assets/scorpion.gif" />
-//     </div>
-// </div>
+arenas.appendChild(createPlayer(player1))
