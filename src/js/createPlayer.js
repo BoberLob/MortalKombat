0e7bc61ca2de0f965c38bc2db29178e31a8c98fc
BoberLob <55806787+BoberLob@users.noreply.github.com>
2021-10-19 23:39:43 +0300

    Update createPlayer.js

--- src/js/createPlayer.js
+++ src/js/createPlayer.js
@@ -1,17 +1,17 @@
 import createElem from ('./createElem')
 
-export default function createPlayer(playerObj) {
+export default function createPlayer(fighters) {
     
-    const $player = createElem('div', 'player' + playerObj.player);
+    const $player = createElem('div', 'player' + fighters.id);
     const $progressbar = createElem('div', 'progressbar');
     const $character = createElem('div', 'character');
     const $life = createElem('div', 'life');
     const $name = createElem('div', 'name');
     const $img = createElem('img');
     
-    $life.style.width = playerObj.hr + "%";
-    $name.innerText = playerObj.name;
-    $img.src = playerObj.img;
+    $life.style.width = fighters.hr + "%";
+    $name.innerText = fighters.name;
+    $img.src = fighters.img;
 
     $player.appendChild($progressbar);
     $player.appendChild($character);
