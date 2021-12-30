52873237eb9a8d455508fc0285c9bfc73c9de82e
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-12-30 10:36:29 +0200

    generateLogs() 'hit', 'defence'

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import createElement from './js/createElement.js';
 
 import { playerAttack, enemyAttack } from './js/attack.js';
 import generateLogs from './js/generateLogs.js';
-
 import obj from './assets/db.js';
+
 const {fighters}=obj
 const player1 = new Player(fighters[0]);
-const player2 = new Player(fighters[4]);
+const player2 = new Player(fighters[1]);
 
 const arenas = document.querySelector('.arenas');
+const randomButton = document.querySelector('.button');
 const formFight = document.querySelector('.control');
 
 
@@ -25,20 +26,28 @@ function showResult(name) {
   return winTitle;
 }
 
+function createReloadButton() {
+  const reloadButton = createElement('button', 'button', 'Restart');
+  const reloadWrap = createElement('div', 'reloadWrap', [reloadButton]);
+
+  reloadButton.addEventListener('click', function() {
+    window.location.reload();
+  });
+  return reloadWrap;
+}
+
 function gameOver() {
-  if (player1.hp < 0 && player2.hp > 0) {
-    player1.hp = 0;
+  if (player2.hp > 0) {
     arenas.appendChild(showResult(player2.name));
     generateLogs('end', player2, player1);
   }
-  if (player1.hp > 0 && player2.hp < 0) {
-    player2.hp = 0;
+
+  if (player1.hp > 0 ) {
     arenas.appendChild(showResult(player1.name));
     generateLogs('end', player1, player2);
   }
-  if (player1.hp < 0 && player2.hp < 0) {
-    player1.hp = 0;
-    player2.hp = 0;
+  if (player1.hp === 0 && player2.hp === 0) {
+
     arenas.appendChild(showResult());
     generateLogs('draw', player1, player2);
   }
@@ -49,45 +58,47 @@ formFight.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const enemy = enemyAttack();
-  const attack = playerAttack();
+  const attack = playerAttack(formFight);
+
+  console.log(attack)
+  console.log(enemy)
+
   let damagePlayer1 = 0;
   let damagePlayer2 = 0;
-
+  let player1HP =100
+  let player2HP =100
 
   if (enemy.hit === attack.defence) {
     generateLogs('defence', player2, player1, damagePlayer1);
   } else {
     damagePlayer1 = enemy.value;
 
-    player1.changeHP(damagePlayer1);
+    player1HP =player1.changeHP(damagePlayer1);
     player1.renderHP();
 
     generateLogs('hit', player2, player1, damagePlayer1);
   }
+
   if (attack.hit === enemy.defence) {
     generateLogs('defence', player1, player2, damagePlayer1);
   } else {
     damagePlayer2 = attack.value;
 
-    player2.changeHP(damagePlayer2);
+    player2HP =player2.changeHP(damagePlayer2);
     player2.renderHP();
 
     generateLogs('hit', player1, player2, damagePlayer2);
   }
-  gameOver();
+
+  if (player1HP === 0 || player2HP === 0) {
+    randomButton.disabled = true;
+    arenas.appendChild(createReloadButton());
+    gameOver();
+  }
 })
 
-function createReloadButton() {
-  const reloadWrap = createElement('div', 'reloadWrap');
-  const reloadButton = createElement('button', 'button');
-  reloadButton.innerHTML = 'Restart';
-  reloadWrap.appendChild(reloadButton);
-  reloadButton.addEventListener('click', function() {
-    window.location.reload();
-  });
-  return reloadWrap;
-}
 
-arenas.appendChild(createPlayer(player2));
+
 arenas.appendChild(createPlayer(player1));
+arenas.appendChild(createPlayer(player2));
 generateLogs('start', player1, player2);
